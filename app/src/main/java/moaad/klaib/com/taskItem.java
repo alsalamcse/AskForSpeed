5c8a514134b6f2f150bf7dc551856133b9a5490a
Owner <ekrmaawad231@gmail.com>
2019-12-16 09:26:06 +0200

    first push

---
+++ app/src/main/java/moaad/klaib/com/taskItem.java
@@ -0,0 +1,14 @@
+package moaad.klaib.com;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class taskItem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_task_item );
+    }
+}
